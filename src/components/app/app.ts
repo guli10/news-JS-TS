5231f4e22cadc14f112c44e1ffb0ad09ec11f880
guli10 <ibragimovaguli62@gmail.com>
2023-02-01 13:22:53 +0500

    final part task

---
+++ src/components/app/app.ts
@@ -0,0 +1,36 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+// class App {
+//     constructor() {
+//         this.controller = new AppController();
+//         this.view = new AppView();
+//     }
+
+//     start() {
+//         document
+//             .querySelector('.sources')
+//             .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+//         this.controller.getSources((data) => this.view.drawSources(data));
+//     }
+// }
+
+class App {
+    public controller: any;
+    public view: any;
+
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        document
+
+            .querySelector('.sources')
+            .addEventListener('click', (e) => this.controller.getNews(e, (data: any) => this.view.drawNews(data)));
+        this.controller.getSources((data: any) => this.view.drawSources(data));
+    }
+}
+
+export default App;
---
+++ src/components/controller/loader.ts
@@ -0,0 +1,94 @@
+// class Loader {
+//     constructor(baseLink, options) {
+//         this.baseLink = baseLink;
+//         this.options = options;
+//     }
+
+//     getResp(
+//         { endpoint, options = {} },
+//         callback = () => {
+//             console.error('No callback for GET response');
+//         }
+//     ) {
+//         this.load('GET', endpoint, callback, options);
+//     }
+
+//     errorHandler(res) {
+//         if (!res.ok) {
+//             if (res.status === 401 || res.status === 404)
+//                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+//             throw Error(res.statusText);
+//         }
+
+//         return res;
+//     }
+
+//     makeUrl(options, endpoint) {
+//         const urlOptions = { ...this.options, ...options };
+//         let url = `${this.baseLink}${endpoint}?`;
+
+//         Object.keys(urlOptions).forEach((key) => {
+//             url += `${key}=${urlOptions[key]}&`;
+//         });
+
+//         return url.slice(0, -1);
+//     }
+
+//     load(method, endpoint, callback, options = {}) {
+//         fetch(this.makeUrl(options, endpoint), { method })
+//             .then(this.errorHandler)
+//             .then((res) => res.json())
+//             .then((data) => callback(data))
+//             .catch((err) => console.error(err));
+//     }
+// }
+
+class Loader {
+    public baseLink: any;
+    public options: any;
+
+    constructor(baseLink: any, options: any) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { options = {} },
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', callback, options);
+    }
+
+    errorHandler(res: any) {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options: any, endpoint: any) {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load(method: any, endpoint: any, callback: any, options = {}) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
